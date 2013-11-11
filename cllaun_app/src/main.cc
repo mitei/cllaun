90960c0cabd232c9b5ce1f839bad9a0e1bff7f6d
mitei <xyou.taf@gmail.com>
2013-11-11 15:46:04 +0900

    change MainWindow to singleton

--- cllaun_app/src/main.cc
+++ cllaun_app/src/main.cc
@@ -1,20 +1,16 @@
 #include <QApplication>
-#include <QMainWindow>
 #include <QFile>
 #include "main_window.h"
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
-    QMainWindow main_w;
-
     QFile main_style_file("assets/style.qss");
     main_style_file.open(QFile::ReadOnly);
     QString str_style = QString::fromUtf8(main_style_file.readAll());
     app.setStyleSheet(str_style);
 
-    cllaun::MainWindow mymain_w(&main_w);
-    mymain_w.move(400, 300);
-    mymain_w.show();
+    cllaun::MainWindow::Instance()->move(400, 300);
+    cllaun::MainWindow::Instance()->show();
 
     return app.exec();
 }
--- cllaun_app/src/main_window.h
+++ cllaun_app/src/main_window.h
@@ -3,14 +3,27 @@
 #include <QWidget>
 #include <QVBoxLayout>
 #include <QLineEdit>
+#include <QMainWindow>
 //#include <QSystemTrayIcon>
 
 namespace cllaun {
     class MainWindow: public QWidget {
         Q_OBJECT
     public:
+        static MainWindow* Instance() {
+            static QMainWindow main_window;
+            static MainWindow mymain_window(&main_window);
+
+            return &mymain_window;
+        }
+        inline QLineEdit* CommandLine() { return &command_line; }
+
+    private:
+        MainWindow() { }
+        MainWindow(const MainWindow& rhs) { }
+        MainWindow& operator=(const MainWindow& rhs) { }
         explicit MainWindow(QWidget* parent = 0);
-        ~MainWindow(){}
+
     private:
         QVBoxLayout layout;
         QLineEdit command_line;
